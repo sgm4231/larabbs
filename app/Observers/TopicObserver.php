524ee6f9e817f9794b34617d91dba2c2761434ed
Your Name <you@example.com>
2019-06-19 15:14:43 +0800

    WYSIWYG 编辑器

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,4 +18,12 @@ class TopicObserver
     {
         //
     }
+
+    //我们要定制此观察器，在 Topic 模型保存时触发的 saving 事件中，
+    //对 excerpt 字段进行赋值
+    //make_excerpt() 是我们自定义的辅助方法，我们需要在 helpers.php 文件中添加
+    public function saving(Topic $topic)
+    {
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
