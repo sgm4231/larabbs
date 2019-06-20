70584eb680e04c51a22d4cdc1261d20837ca60d8
Your Name <you@example.com>
2019-06-20 09:58:19 +0800

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -67,5 +67,11 @@ class Topic extends Model
         return $this->hasMany(Reply::class);
     }
 
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
+
 }
 
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -30,11 +30,15 @@ class ReplyObserver
         //上面自增 1 是比较直接的做法，另一个比较严谨的做法是创建成功后计算本话题下评论总数，
         //然后在对其 reply_count 字段进行赋值。这样做的好处多多，一般在做 xxx_count 此类总数缓存字段时，推荐使用此方法：
         //$reply->topic->increment('reply_count', 1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         // 通知话题作者有新的评论
         //默认的 User 模型中使用了 trait —— Notifiable，它包含着一个可以用来发通知的方法 notify() ，此方法接收一个通知实例做参数。虽然 notify() 已经很方便，但是我们还需要对其进行定制，我们希望每一次在调用 $user->notify() 时，自动将 users 表里的 notification_count +1 ，这样我们就能跟踪用户未读通知了。
         $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
