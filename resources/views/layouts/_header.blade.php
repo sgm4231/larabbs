5d16c46ea5650f4081425f7aadafe46d4f840904
Your Name <you@example.com>
2019-06-20 18:15:31 +0800

    集成 administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -33,6 +33,7 @@
                     <li class="nav-item notification-badge">
                         <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
                             {{ Auth::user()->notification_count }}
+                            {{ Auth::user()->notification_count }}
                         </a>
                     </li>
                     <li class="nav-item dropdown">
@@ -41,6 +42,13 @@
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @can('manage_contents')
+                                <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+                                    <i class="fas fa-tachometer-alt mr-2"></i>
+                                    管理后台
+                                </a>
+                                <div class="dropdown-divider"></div>
+                            @endcan
                             <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
                                 <i class="far fa-user mr-2"></i>
                                 个人中心
